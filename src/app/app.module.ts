510a899f4b615a3d0dc756592647d6b02b7a67e7
mundo316 <raymundo.garcia316@yahoo.com>
2020-01-14 19:37:57 -0600

    progress!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductDetailComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
